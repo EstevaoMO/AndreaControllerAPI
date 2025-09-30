aa2a0e8eaf37d88730628f9c78ad551f42db17cf
EstevaoMO <estevaomoraes48@gmail.com>
2025-09-30 12:24:15 -0300

    atualizações para deploy onrender

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
+from fastapi.responses import JSONResponse
+from supabase import create_client
 from routers import chamadas, revistas, vendas
+
+from settings.settings import importar_configs
+from services.auth import pegar_usuario_admin
 
 # Configurações iniciais
 app = FastAPI(
     title="AndreaController API's Swagger",
     tags=["Global"]
 )
+
+st = importar_configs()
 
 # Rotas globais
 @app.get("/")
@@ -14,7 +21,23 @@ def home():
 
 @app.get("/ping")
 def ping():
+    supabase = create_client(s)
     return { "ping": "pong!" }
+
+@app.head("/ping", status_code=status.HTTP_200_OK)
+def ping():
+    try:
+        con = pegar_usuario_admin()
+        if con:
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content="Pong!"
+            )
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_BAD_REQUEST,
+            detail=f"Algo de errado aconteceu: {str(e)}"
+        )
 
 # Outras rotas
 app.include_router(chamadas.router)
